ee3c99df29f614d858e80452e1ae69ab3fd61f65
taina <tainasantosbc@gmail.com>
2024-04-25 22:53:53 -0300

    adicionando funcionalidade a PokemonList.tsx

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -1,18 +1,22 @@
 import exp from "constants";
 import React, { useEffect, useState } from "react";
 import PokemonService from "../../services/PokemonServices";
-import { PokemonListInterface } from "../../types/AppInterfaces";
+import { PokemonItemListInterface, PokemonListInterface } from "../../types/AppInterfaces";
 
 const PokemonList: React.FC = () => {
     
     const [pagina, setPagin] = useState<number>(1);
     const [itensPorPagina, setItensPorPagina] = useState<number>(20);
+    const [PokemonList, setPokemonList] = useState<PokemonItemListInterface[]>([]);
 
     const handleListarDados = async () => {
         const responseData: PokemonListInterface = await PokemonService.listarPokemons({
             itensPorPagina: itensPorPagina,
             pagina: pagina
         });
+        if (responseData.results.length > 0){
+            setPokemonList(responseData.results);
+        }
     }
 
     useEffect( () => {
